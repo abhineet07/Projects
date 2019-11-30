0fcd3358a4618296476404444ace1d258e212698
Abhineet Gupta <abhineet2310@gmail.com>
2019-11-30 15:44:56 +0530

    using url names

--- BlogWebsite1/django_project/blog/urls.py
+++ BlogWebsite1/django_project/blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	# the 'name' will be used for navigation from on page to another inside href
+	# example : <a class="nav-item nav-link" href="{% url 'blog-home' %}">Home</a> 
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
 ]
