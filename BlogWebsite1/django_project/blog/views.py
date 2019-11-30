6798b8e533a9b99520cd9a77ed99a295a1575f3b
Abhineet Gupta <abhineet2310@gmail.com>
2019-11-30 20:56:54 +0530

    database migrations

--- BlogWebsite1/django_project/blog/views.py
+++ BlogWebsite1/django_project/blog/views.py
@@ -1,33 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-	{
-		'author' : 'Abhineet',
-		'title' : 'Blog Post 1',
-		'content' : 'First blog content',
-		'date_posted' : 'November 30'
-	},
-	{
-		'author' : 'Deepak',
-		'title' : 'Blog Post 2',
-		'content' : 'Second blog content',
-		'date_posted' : 'November 30'
-	},
-	{
-		'author' : 'Navneet',
-		'title' : 'Blog Post 3',
-		'content' : 'Third blog content',
-		'date_posted' : 'November 30'
-	}
-]
+from .models import Post
 
 # 1)this functions handle traffic from home page
 # we also need to map urlpattern to this function
 # for this we create a urls.py file within this app
 def home(request):
 	context = {
-		'posts' : posts
+		'posts' : Post.objects.all()
 	}
 	# return HttpResponse('<h1>Blog Home</h1>')
 	return render(request, 'blog/home.html', context)
